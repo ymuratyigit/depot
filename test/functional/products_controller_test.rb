5b4edecd91566fcbeb48c18b21d3be255d92c51d
Yasin Murat Yigit <ymuratyigit@gmail.com>
2011-10-06 00:47:20 +0300

    Finished Catalog Display!

--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -4,10 +4,10 @@ class ProductsControllerTest < ActionController::TestCase
   setup do
     @product = products(:one)
     @update = {
-      :title => 'Lorem Ipsum',
+      :title       => 'Lorem Ipsum',
       :description => 'Wibbles are fun!',
-      :image_url => 'lorem.jpg',
-      :price
+      :image_url   => 'lorem.jpg',
+      :price       => 19.95 
     }
   end
 
